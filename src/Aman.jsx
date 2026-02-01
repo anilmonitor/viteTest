94d4d1ffffdd2136fed78ee59e9f4ca83a556cab
ANIL <anilarangi6@gmail.com>
2026-02-01 20:03:10 +0530

    Aman profile add

--- src/Aman.jsx
+++ src/Aman.jsx
@@ -1,21 +1,29 @@
-import "./Aditya.css"
-
-// import "./Aditya.css";
+import "./Aman.css";
+import amanImg from "./assets/aman.jpg";
 
 function AMAN() {
+  return (
+    <div className="profile-wrapper">
+      <div className="profile-card">
+        <img src={amanImg} alt="Aman profile" className="profile-img" />
 
+        <h1>Aman Sharma</h1>
+        <p className="role">CSE Student • Mentor</p>
 
-    return (
-        <div className="aditya-container">
+        <p className="bio">
+          My name is Aman Sharma. I am currently pursuing my B.Tech degree
+          from Ramgarh Engineering College. My branch is CSE and my hobby
+          is singing.
+        </p>
 
-            <h1>About Aman</h1>
-    
-            <p>My name is AMAN Sharma currently I am pursuing my b tech degree from Ramgarh engineering college and My branh is CSE and my hpbby is singing </p>
+        <div className="social-links">
+          <a href="https://instagram.com/" target="_blank">Instagram</a>
+          <a href="https://github.com/" target="_blank">GitHub</a>
+          <a href="https://facebook.com/" target="_blank">Facebook</a>
         </div>
-    )
-
-        ;
+      </div>
+    </div>
+  );
 }
-
 
 export default AMAN;
