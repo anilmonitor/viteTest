4de96d289711fa07607324a3e000cbbe1f8e263f
ANIL <anilarangi6@gmail.com>
2026-02-06 14:18:01 +0530

    Pass array to props

--- src/components/amazonCard/Card.jsx
+++ src/components/amazonCard/Card.jsx
@@ -1,13 +1,16 @@
 
 import CardTemplate from "./CardTemplate";
-let Card = () => {
 
+
+let Card = () => {
+let ar = ["Anil", "Rahul"];
     return <>
 
 
-        <CardTemplate heading="Realme X7 max" price={30000} about="I am Card " />
-        <CardTemplate heading="Laptop" price={59450} about="I am laptop " />
+        <CardTemplate heading="Realme X7 max" price={30000} about="I am Card " list={ar}/>
+        <CardTemplate heading="Laptop" price={59450} about="I am laptop "  list={ar}/>
 
+  <CardTemplate heading="IQOO 9" price={75400} about="This is IQOO new 7 phone and this phone is belong to Ranjeet"  list={ar}/>
 
     </>
 
--- src/components/amazonCard/CardTemplate.jsx
+++ src/components/amazonCard/CardTemplate.jsx
@@ -1,11 +1,12 @@
 import "./Card.css"
-let CardTemplate = ({ heading, about, price }) => {
+let CardTemplate = ({ heading, about, price , list}) => {
     return <>
 
         <div className="card">
             <h1>{heading}</h1>
             <p>{about}</p>
             <p>{price}</p>
+            <p>{list}</p>
         </div>
 
     </>
