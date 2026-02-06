e4ed4bab54e0d054197787b82f1fa15f76704744
ANIL <anilarangi6@gmail.com>
2026-02-07 02:57:47 +0530

    Change photo extension

--- src/components/members/Members.jsx
+++ src/components/members/Members.jsx
@@ -15,7 +15,7 @@ function Members() {
                 <Member name="Soni Kumari" age={20} img="https://recstudentportal.com/udaanmembers/profile/aman.jpeg" branch="CSE" />
                 <Member name="ANIL  Kumar" age={22} img="https://recstudentportal.com/udaanmembers/profile/alok.jpeg" branch="CSE" />
 
-                <Member name="Ankit  Mandal" age={21} img="src/assets/mandal.jpeg" branch="CSE" gender="Male" email="bm@gmail.com" />
+                <Member name="Ankit  Mandal" age={21} img="src/assets/mandal.jpg" branch="CSE" gender="Male" email="bm@gmail.com" />
 
 
                 <Member name="Soni Kumari" age={20} img="https://recstudentportal.com/udaanmembers/profile/soni.jpeg" branch="CSE" />
