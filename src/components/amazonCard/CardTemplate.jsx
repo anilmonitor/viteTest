d649676be17cefa449a8316fcc5a41575f9a8fca
ANIL <anilarangi6@gmail.com>
2026-02-07 02:49:41 +0530

    ADD style based on condition

--- src/components/amazonCard/CardTemplate.jsx
+++ src/components/amazonCard/CardTemplate.jsx
@@ -1,16 +1,23 @@
 import "./Card.css"
 let CardTemplate = ({ heading, about, price, list, obj }) => {
 
-    // let singleItem = list.map((single) => <li>{single}</li>);
+    //    let style =  {   backgroundColor: price > 30000 ? "green" color: "white":  "" };
+
+    let style = {
+        backgroundColor: price > 30000 ? "green" : "",
+        color: price > 30000 ? "white" : ""
+    };
+
 
     return <>
 
-        <div className="card">
+
+        <div className="card" >
             <h1>{heading}</h1>
             <p>{about}</p>
             <p>{price}</p>
             {/* { price > 30000 && <p>5% discount</p>} */}
-            {price > 30000 ? <p>5% discount</p> : <a href="#">Get discount</a>}
+            {price > 30000 ? <p style={style}>5% discount</p> : <a href="#">Get discount</a>}
 
 
             {/* for rendering array  */}
