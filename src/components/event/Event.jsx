89744b0e83e7c986c8fcdb62c54e7e10de0bfe2b
ANIL <anilarangi6@gmail.com>
2026-02-15 03:08:10 +0530

    More event

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -8,12 +8,21 @@ function welcome() {
     console.log(`Hello ${name} Welcome!!`);
 }
 
+function handleHover(){
+    console.log("You hover paragraph");
+}
+
+function handleDoubleClick(){
+    console.log("You double click me!!");
+}
 export default function Event() {
     return (
-        <div className="cont">
+        <div className="cont" style={{color: "red"}}>
             <button onClick={hello}>Click me !!</button>
-
             <p onClick={welcome} style={{ color: "white" }}>Click me to Greet</p>
+
+            <p onMouseOver={handleHover}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, magnam.</p>
+            <button onDoubleClick={handleDoubleClick}>Double click</button>
         </div>
     )
 };
