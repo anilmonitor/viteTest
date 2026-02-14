442b65100441306bcc1b97d3acae6bc41633978a
ANIL <anilarangi6@gmail.com>
2026-02-14 23:36:39 +0530

    reduce() Method

--- basic.js
+++ basic.js
@@ -9,18 +9,30 @@ let res = array.map(ok);
 // console.log(res)
 
 // or  --> via arrow function
-let res2 = array.map(e => e*2);
+let res2 = array.map(e => e * 2);
 // console.log(res2);
 
 
 
 // filter() method 
-let filres = array.filter(e => (e%2 != 0)); //give odd number
-console.log(filres);
+let filres = array.filter(e => (e % 2 != 0)); //give odd number
+// console.log(filres);
 
 // or 
-function greaterthan4(x){
-    return x>4; //it will filter the all value greate then 4 and give us
+function greaterthan4(x) {
+    return x > 4; //it will filter the all value greate then 4 and give us
 }
 let filres2 = array.filter(greaterthan4);
-console.log(filres2);
+// console.log(filres2);
+
+
+
+// reduce() method 
+let reduceRes = array.reduce(function (MAX, current) {
+
+    if (current > MAX) {
+        MAX = current;
+    }
+    return MAX;
+}, 0);
+console.log(reduceRes)
