97e9fd7d58d01d1f5f4ded004f322dfe7478b786
ANIL <anilarangi6@gmail.com>
2026-02-15 13:38:28 +0530

    Keyboard event

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -8,22 +8,34 @@ function welcome() {
     console.log(`Hello ${name} Welcome!!`);
 }
 
-function handleHover(){
+function handleHover() {
     console.log("You hover paragraph");
 }
 
-function handleDoubleClick(){
+function handleDoubleClick() {
     console.log("You double click me!!");
 
 }
+
+function handleKeyDown(e){
+    console.log("Pressed key:", e.key);
+};
+
+
 export default function Event() {
     return (
-        <div className="cont" style={{color: "red"}}>
+        <div className="cont" style={{ color: "red" }}>
             <button onClick={hello}>Click me !!</button>
             <p onClick={welcome} style={{ color: "white" }}>Click me to Greet</p>
 
             <p onMouseOver={handleHover}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, magnam.</p>
             <button onDoubleClick={handleDoubleClick}>Double click</button>
+
+            <input
+                type="text"
+                onKeyDown={handleKeyDown}
+                placeholder="Type something..."
+            />
         </div>
     )
 };
